f56b8577aef9329ad8173ec69593b2375fd1487e
Zeenat Azeem <125384808+zeenatazeem-dev@users.noreply.github.com>
2023-05-21 04:14:54 -0700

    HI CHECK MY WORK .

---
+++ Assignments/Assignment-1 - Copy/app.js
@@ -0,0 +1,32 @@
+
+//("hello") 
+//ALERT 
+//1. Write a script to greet your website visitor using JS alert box.
+//alert("Hi! I am Zeenat nice to meet you");
+  
+  //2. Write a script to display following message on your web 
+//page:
+//alert("JavaScript\nError! Please enter a valid password.");
+
+//3. Write a script to display following message on your web 
+//page: (Hint : Use line break)
+//alert("JavaScript Alert \n Welcome to JS Land...\n Happy Codding!");
+
+//4. Write a script to display following messages in sequence: 
+ //alert("JavaScript Alert \n Welcom to JS Land...");                                                       
+//alert("JavaScript Alert \n Happy Coding!  \n please this page from creating additional dialogds");
+
+//5. Generate the following message through browser’s 
+//developer console:
+//alert("JavaScript Alert \n Hello...I can run JS through my web browser's console ")
+
+//6. Make use of alerts in your new/existing HTML & CSS 
+//project.
+/*. Practice placement of <script></script> element in 
+following sections of your project in exercise 6:
+a. Head
+b. Body (before your page’s HTML)
+ALERTS | JAVASCRIPT
+Page 3 of 3
+c. Body (inside your page’s HTML)
+d. Body (after your page’s HTML*/
